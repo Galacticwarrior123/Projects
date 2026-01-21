f73394becbac515cc44126117dcaa83951d42a2f
Galacticwarrior123 <simonmurguri@gmail.com>
2026-01-21 23:05:31 +0630

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-medical_cause = input("Did you have a medical condition? (yes/no) ")
-atten=int(input("enter the attendance of the student: "))
-if medical_cause == "yes":
-    if atten < 75:
-        print("You are not eligible for the exam.")
-    else:
-        print("You are eligible for the exam.")
-else:
-    if atten < 75:
-        print("You are not eligible for the exam.")
-    else:
-        print("You are eligible for the exam.")
+n = int(input("Enter a number: "))
+sum = 0
+for i in range(1, n + 1):
+    sum += i
+print("Sum =", sum)
