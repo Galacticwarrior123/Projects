78495c0b844528108a5178d931ed82b1936f3831
Galacticwarrior123 <simonmurguri@gmail.com>
2026-03-11 23:16:47 +0630

    Add files via upload

--- Main2.py
+++ Main2.py
@@ -1,18 +1,9 @@
-try:
-    num1,num2 = eval(input("Enter two numbers separated by a comma: "))
-    result = num1 / num2
-    print("The result is:", result)
-except ZeroDivisionError as ex:
-       print("Error: Division by zero is not allowed.")
-       print("Error details:", ex)
-except SyntaxError as ex:
-       print("Error: Invalid input syntax.")
-       print("Error details:", ex)
-       print("Please enter the numbers in the correct format. (e.g., 1, 2)")
-except:
-      print("Wrong input.")
-else:
-      print("No exceptions")
-finally:
-      print("This will print no matter what.")
+setx = {"green","yellow"}
+sety = {"blue","yellow"}
 
+print("Original set :")
+print(setx)
+print(sety)
+print("\nIntersection of two said sets:")
+setz = setx.intersection(sety)
+print(setz)
