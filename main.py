4f26b872da55edb1776a3c344887f8134373e38a
Galacticwarrior123 <simonmurguri@gmail.com>
2026-03-18 23:25:02 +0630

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,54 @@
-student_data= {
-    "id1": {"name": "John Doe", "age": 20, "major": "Computer Science"},
-    "id2": {"name": "Jane Smith", "age": 22, "major": "Mathematics"},
-    "id3": {"name": "Alice Johnson", "age": 21, "major": "Physics"},
-    "id4": {"name": "Bob Brown", "age": 23, "major": "Chemistry"},
-}
+import random
+import time
 
-result = {}
-seen_key = []
+number = random.randint(1, 100)
 
-for student_id,details in student_data.items():
-    unique_key = (details["name"], details["age"], details["major"])
-    if unique_key not in seen_key:
-        seen_key.append(unique_key)
-        result[student_id]=details
+def intro():
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    print("Can you guess what it is?")
+    name = input("What's your name? ")
+    if (number%2==0):
+        x = "even"
+    else:
+        x = "odd"
+    print("\nThis is {} number.".format(x))
+    time.sleep(.5)
+    print("Go ahead . Guess!")
 
-for k,v in result.items():
-    print(k,":",v)
+def pick():
+    guessesTaken = 0
+    while guessesTaken < 6:
+        time.sleep(.25)
+        enter = input("Enter your guess: ")
+        try:
+            guess = int(enter)
+            if guess <= 100 and guess >= 1:
+                guessesTaken = guessesTaken + 1
+            if guessesTaken < 6:
+                if guess < number:
+                    print("Your guess is too low.")
+                if guess > number:
+                    print("Your guess is too high.")
+                if guess != number:
+                    time.sleep(.5)
+                    print("Try again.")
+                if guess == number:
+                    break
+            if guess > 100 or guess < 1:
+                    print("Please enter a number between 1 and 100.")
+                    time.sleep(.25)
+                    print("Try again.")
+        except:
+            print("Please enter a valid number.")
+    if guess == number:
+        guessesTaken = str(guessesTaken)
+        print("Congratulations! You guessed the number in {} tries.".format(guessesTaken))
+    if guess != number:
+        print("Sorry, you didn't guess the number. The number was {}.".format(number))
+
+playagain = "yes"
+while playagain == "yes" or playagain == "y" or playagain == "Yes":
+    intro()
+    pick()
+    playagain = input("Do you want to play again?")
